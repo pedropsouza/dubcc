3b83910b25e61cf0ba88a17b81a8f4cf9757d362
guiganassini17@gmail.com <guiganassini17@gmail.com>
2025-06-12 11:26:17 -0300

    read asm from file implementation

--- assembler.go
+++ assembler.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"regexp"
 	"slices"
+	"bytes"
 )
 
 type (
@@ -226,6 +227,16 @@ func (info *Info) registerConst(name string, val MachineWord) {
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
+	if len(os.Args) == 2 {
+		inputFile, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			log.Println(err)
+		}
+		r := bytes.NewReader(inputFile)
+		reader = bufio.NewReader(r)
+	}
+
 	info := Info {
 		isa: ISA {
 			map[string]Instruction {
@@ -265,7 +276,7 @@ func main() {
 		},
 		symbols: make(map[string]MachineAddress),
 	}
-
+	
 	for {
 		line, err := reader.ReadString('\n')
 		line = strings.TrimSpace(line)
---
+++ test.asm
@@ -0,0 +1,4 @@
+add 20
+copy 30 20
+sub 8
+load 2
