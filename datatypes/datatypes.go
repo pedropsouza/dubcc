25ab6edec6f8e768d2a8c879ad4805ea06035de3
Lobke <stephan.heideman@gmail.com>
2025-06-18 16:47:49 -0300

    New Instructions.

--- datatypes/datatypes.go
+++ datatypes/datatypes.go
@@ -2,19 +2,19 @@ package datatypes
 
 type (
 	MachineAddress = uint64
-	MachineWord = uint16
+	MachineWord    = uint16
 )
 
 type ISA struct {
 	Instructions map[string]Instruction
-	Registers map[string]*Register
-	MOT map[MachineWord]Instruction
+	Registers    map[string]*Register
+	MOT          map[MachineWord]Instruction
 }
 
 type Instruction struct {
-	Name string
+	Name    string
 	NumArgs int
-	Repr MachineWord
+	Repr    MachineWord
 }
 
 const (
@@ -24,6 +24,7 @@ const (
 )
 
 type RegisterTag byte
+
 const (
 	RegisterTagGeneralPurpose = 1 << iota
 	RegisterTagSpecial
@@ -31,30 +32,30 @@ const (
 )
 
 type Register struct {
-	Name string
-	Desc string
-	Size uint
+	Name     string
+	Desc     string
+	Size     uint
 	Longdesc string
-	Tags RegisterTag
-	Content MachineWord
+	Tags     RegisterTag
+	Content  MachineWord
 }
 
-func GetDefaultISA () ISA {
-	insts := map[string]Instruction {
-		"add":   Instruction { Name: "add", NumArgs: 1, Repr: 2 },
-		"br":    Instruction { Name: "br", NumArgs: 1, Repr: 0 },
-		"brneg": Instruction { Name: "brneg", NumArgs: 1, Repr: 5 },
-		"brpos": Instruction { Name: "brpos", NumArgs: 1, Repr: 1 },
-		"brzero":Instruction { Name: "brzero", NumArgs: 1, Repr: 4 },
-		"copy":  Instruction { Name: "copy", NumArgs: 2, Repr: 13 },
-		"divide":Instruction { Name: "divide", NumArgs: 1, Repr: 10 },
-		"load":  Instruction { Name: "load", NumArgs: 1, Repr: 3 },
-		"mult":  Instruction { Name: "mult", NumArgs: 1, Repr: 14 },
-		"read":  Instruction { Name: "read", NumArgs: 1, Repr: 12 },
-		"stop":  Instruction { Name: "stop", NumArgs: 0, Repr: 11 },
-		"store": Instruction { Name: "store", NumArgs: 1, Repr: 7 },
-		"sub":   Instruction { Name: "sub", NumArgs: 1, Repr: 6 },
-		"write": Instruction { Name: "write", NumArgs: 1, Repr: 8 },
+func GetDefaultISA() ISA {
+	insts := map[string]Instruction{
+		"add":    Instruction{Name: "add", NumArgs: 1, Repr: 2},
+		"br":     Instruction{Name: "br", NumArgs: 1, Repr: 0},
+		"brneg":  Instruction{Name: "brneg", NumArgs: 1, Repr: 5},
+		"brpos":  Instruction{Name: "brpos", NumArgs: 1, Repr: 1},
+		"brzero": Instruction{Name: "brzero", NumArgs: 1, Repr: 4},
+		"copy":   Instruction{Name: "copy", NumArgs: 2, Repr: 13},
+		"divide": Instruction{Name: "divide", NumArgs: 1, Repr: 10},
+		"load":   Instruction{Name: "load", NumArgs: 1, Repr: 3},
+		"mult":   Instruction{Name: "mult", NumArgs: 1, Repr: 14},
+		"read":   Instruction{Name: "read", NumArgs: 1, Repr: 12},
+		"stop":   Instruction{Name: "stop", NumArgs: 0, Repr: 11},
+		"store":  Instruction{Name: "store", NumArgs: 1, Repr: 7},
+		"sub":    Instruction{Name: "sub", NumArgs: 1, Repr: 6},
+		"write":  Instruction{Name: "write", NumArgs: 1, Repr: 8},
 	}
 
 	mot := make(map[MachineWord]Instruction)
@@ -62,49 +63,49 @@ func GetDefaultISA () ISA {
 		mot[inst.Repr] = inst
 	}
 
-	return ISA {
+	return ISA{
 		Instructions: insts,
-		MOT: mot,
-		Registers: map[string]*Register {
-			"PC": &Register {
+		MOT:          mot,
+		Registers: map[string]*Register{
+			"PC": &Register{
 				Name: "PC", Desc: "Contador de Instruções (Program Counter)", Size: 16,
 				Longdesc: "Mantém o endereço da próxima instrução a ser executada",
-				Tags: RegisterTagSpecial,
+				Tags:     RegisterTagSpecial,
 			},
-			"SP": &Register {
+			"SP": &Register{
 				Name: "SP", Desc: "Ponteiro de pilha (Stack Pointer)", Size: 16,
 				Longdesc: "Aponta para o topo da pilha do sistema; tem incremento/decremento automático (push/pop)",
-				Tags: RegisterTagSpecial,
+				Tags:     RegisterTagSpecial,
 			},
-			"ACC": &Register {
+			"ACC": &Register{
 				Name: "ACC", Desc: "Acumulador", Size: 16,
 				Longdesc: "Armazena os dados (carregados e resultantes) das operações da Unid. de Lógica e Aritmética",
-				Tags: RegisterTagGeneralPurpose | RegisterTagSpecial,
+				Tags:     RegisterTagGeneralPurpose | RegisterTagSpecial,
 			},
-			"MOP": &Register {
+			"MOP": &Register{
 				Name: "MOP", Desc: "Modo de Operação", Size: 8,
 				Longdesc: "Armazena o indicador do modo de operação, que é alterado apenas por painel de operação (via console de operação - interface visual)",
-				Tags: RegisterTagSpecial,
+				Tags:     RegisterTagSpecial,
 			},
-			"RI": &Register {
+			"RI": &Register{
 				Name: "RI", Desc: "Registrador de Instrução", Size: 16,
 				Longdesc: "Mantém o opcode da instrução em execução (registrador interno)",
-				Tags: RegisterTagSpecial | RegisterTagInternal,
+				Tags:     RegisterTagSpecial | RegisterTagInternal,
 			},
-			"RE": &Register {
+			"RE": &Register{
 				Name: "RE", Desc: "Registrador de Endereço de Memória", Size: 16,
 				Longdesc: "Mantém o endereço de acesso à memória de dados (registrador interno)",
-				Tags: RegisterTagSpecial | RegisterTagInternal,
+				Tags:     RegisterTagSpecial | RegisterTagInternal,
 			},
-			"R0": &Register {
+			"R0": &Register{
 				Name: "R0", Desc: "Registrador de Propósito Geral", Size: 16,
 				Longdesc: "Registrador de Propósito Geral",
-				Tags: RegisterTagGeneralPurpose,
+				Tags:     RegisterTagGeneralPurpose,
 			},
-			"R1": &Register {
+			"R1": &Register{
 				Name: "R1", Desc: "Registrador de Propósito Geral", Size: 16,
 				Longdesc: "Registrador de Propósito Geral",
-				Tags: RegisterTagGeneralPurpose,
+				Tags:     RegisterTagGeneralPurpose,
 			},
 		},
 	}
@@ -120,16 +121,17 @@ func (isa *ISA) InstructionFromWord(
 	return inst, found
 }
 
-type InstHandler func (*Sim, []MachineWord)
+type InstHandler func(*Sim, []MachineWord)
 
 type Sim struct {
-	Mem SimMem
+	Mem      SimMem
 	Handlers map[MachineWord]InstHandler
-	Isa ISA
-	State SimState
+	Isa      ISA
+	State    SimState
 }
 
 type SimState = byte
+
 const (
 	SimStateHalt = iota
 	SimStateRun
@@ -153,8 +155,8 @@ func IsIndirectB(op MachineWord) bool {
 }
 
 func (reg *Register) MapUnary(
-	mapfunc func (MachineWord, MachineWord) MachineWord,
-  value MachineWord) {
+	mapfunc func(MachineWord, MachineWord) MachineWord,
+	value MachineWord) {
 	reg.Content = mapfunc(reg.Content, value)
 }
 
@@ -183,28 +185,44 @@ func (s *Sim) ResolveAddressMode(opword MachineWord, args []MachineWord) (a Mach
 }
 
 func MakeSim(memSize MachineAddress) Sim {
-	instHandlers := map[string]InstHandler {
-		"add": func (s *Sim, args []MachineWord) {
+	instHandlers := map[string]InstHandler{
+		"add": func(s *Sim, args []MachineWord) {
 			opword := args[0]
 			value, _ := s.ResolveAddressMode(opword, args[1:])
 			s.Isa.Registers["ACC"].MapUnary(
-				func (acc, val MachineWord) MachineWord {
+				func(acc, val MachineWord) MachineWord {
 					return acc + val
 				},
 				value,
 			)
 		},
-		"sub": func (s *Sim, args []MachineWord) {
+		"sub": func(s *Sim, args []MachineWord) {
 			opword := args[0]
 			value, _ := s.ResolveAddressMode(opword, args[1:])
 			s.Isa.Registers["ACC"].MapUnary(
-				func (acc, val MachineWord) MachineWord {
+				func(acc, val MachineWord) MachineWord {
 					return acc - val
 				},
 				value,
 			)
 		},
-		"br": func (s *Sim, args []MachineWord) {
+		"divide": func(s *Sim, args []MachineWord) {
+			opword := args[0]
+			value, _ := s.ResolveAddressMode(opword, args[1:])
+			s.Isa.Registers["ACC"].MapUnary(
+				func(acc, val MachineWord) MachineWord { return acc / val },
+				value,
+			)
+		},
+		"mult": func(s *Sim, args []MachineWord) {
+			opword := args[0]
+			value, _ := s.ResolveAddressMode(opword, args[1:])
+			s.Isa.Registers["ACC"].MapUnary(
+				func(acc, val MachineWord) MachineWord { return acc * val },
+				value,
+			)
+		},
+		"br": func(s *Sim, args []MachineWord) {
 			opword := args[0]
 			// if not indirect, must be treated as immediate else labels break
 			if !(IsIndirectA(opword) || IsIndirectB(opword)) {
@@ -212,13 +230,13 @@ func MakeSim(memSize MachineAddress) Sim {
 			}
 			value, _ := s.ResolveAddressMode(opword, args[1:])
 			s.Isa.Registers["PC"].MapUnary(
-				func (pc, target MachineWord) MachineWord {
+				func(pc, target MachineWord) MachineWord {
 					return target
 				},
 				value,
 			)
 		},
-		"brpos": func (s *Sim, args []MachineWord) {
+		"brpos": func(s *Sim, args []MachineWord) {
 			opword := args[0]
 			// if not indirect, must be treated as immediate else labels break
 			if !(IsIndirectA(opword) || IsIndirectB(opword)) {
@@ -226,9 +244,9 @@ func MakeSim(memSize MachineAddress) Sim {
 			}
 			value, _ := s.ResolveAddressMode(opword, args[1:])
 			s.Isa.Registers["PC"].MapUnary(
-				func (pc, target MachineWord) MachineWord {
+				func(pc, target MachineWord) MachineWord {
 					acc_v := s.Isa.Registers["ACC"].Content
-					if acc_v != 0 && (acc_v & 0x8000) == 0 {
+					if acc_v != 0 && (acc_v&0x8000) == 0 {
 						return target
 					} else {
 						return pc
@@ -237,7 +255,7 @@ func MakeSim(memSize MachineAddress) Sim {
 				value,
 			)
 		},
-		"brneg": func (s *Sim, args []MachineWord) {
+		"brneg": func(s *Sim, args []MachineWord) {
 			opword := args[0]
 			// if not indirect, must be treated as immediate else labels break
 			if !(IsIndirectA(opword) || IsIndirectB(opword)) {
@@ -245,7 +263,7 @@ func MakeSim(memSize MachineAddress) Sim {
 			}
 			value, _ := s.ResolveAddressMode(opword, args[1:])
 			s.Isa.Registers["PC"].MapUnary(
-				func (pc, target MachineWord) MachineWord {
+				func(pc, target MachineWord) MachineWord {
 					if (s.Isa.Registers["ACC"].Content & 0x8000) > 0 {
 						return target
 					} else {
@@ -255,7 +273,7 @@ func MakeSim(memSize MachineAddress) Sim {
 				value,
 			)
 		},
-		"brzero": func (s *Sim, args []MachineWord) {
+		"brzero": func(s *Sim, args []MachineWord) {
 			opword := args[0]
 			// if not indirect, must be treated as immediate else labels break
 			if !(IsIndirectA(opword) || IsIndirectB(opword)) {
@@ -263,7 +281,7 @@ func MakeSim(memSize MachineAddress) Sim {
 			}
 			value, _ := s.ResolveAddressMode(opword, args[1:])
 			s.Isa.Registers["PC"].MapUnary(
-				func (pc, target MachineWord) MachineWord {
+				func(pc, target MachineWord) MachineWord {
 					if s.Isa.Registers["ACC"].Content == 0 {
 						return target
 					} else {
@@ -273,30 +291,48 @@ func MakeSim(memSize MachineAddress) Sim {
 				value,
 			)
 		},
-		"stop": func (s *Sim, args []MachineWord) {
+		"load": func(s *Sim, args []MachineWord) {
+			opword := args[0]
+			value, _ := s.ResolveAddressMode(opword, args[1:])
+			s.Isa.Registers["ACC"].MapUnary(
+				func(acc, val MachineWord) MachineWord { return value },
+				value,
+			)
+		},
+		"store": func(s *Sim, args []MachineWord) {
+			opword := args[0]
+			value, _ := s.ResolveAddressMode(opword, args[1:])
+			s.Mem.Work[value] = s.Isa.Registers["ACC"].Content
+		},
+		"stop": func(s *Sim, args []MachineWord) {
 			s.State = SimStateHalt
 		},
-		"copy": func (s *Sim, args []MachineWord) {
+		"copy": func(s *Sim, args []MachineWord) {
 			opword := args[0]
-			a := args[1]
-			b := args[2]
+			l, r := s.ResolveAddressMode(opword, args[1:])
+			s.Mem.Work[l] = s.Mem.Work[r]
 		},
-		"push": func (s *Sim, args []MachineWord) {
-
+		"push": func(s *Sim, args []MachineWord) {
+			opword := args[0]
+			value, _ := s.ResolveAddressMode(opword, args[1:])
+			s.Isa.Registers["ACC"].MapUnary(
+				func(acc, val MachineWord) MachineWord { return acc / val },
+				value,
+			)
 		},
 	}
-
 	isa := GetDefaultISA()
+	isa.Registers["SP"].Content = MachineWord(memSize - 1)
 	mopHandlers := make(map[MachineWord]InstHandler)
 	for name, handler := range instHandlers {
 		mopHandlers[isa.Instructions[name].Repr] = handler
 	}
 
-	return Sim {
-		Mem: SimMem {
+	return Sim{
+		Mem: SimMem{
 			Work: make([]MachineWord, memSize),
 		},
-		Isa: isa,
+		Isa:      isa,
 		Handlers: mopHandlers,
 	}
 }
